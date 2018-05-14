cbd43cb742b939584c395a3ea890012f5aeda4dc
GagaNinja <vivasm@gmail.com>
2018-05-14 12:34:41 -0300

    Calculo do Gamma

--- main.cpp
+++ main.cpp
@@ -7,6 +7,11 @@
 #include "worker.h"
 
 using namespace std;
+inline bool exists_test (const std::string& name) {
+    ifstream f(name.c_str());
+    return f.good();
+}
+
 
 int main(int argc, char *argv[])
 {
@@ -14,15 +19,17 @@ int main(int argc, char *argv[])
     clock_t ti,tf;
     int popS=atoi(argv[1]);
     int popT=atoi(argv[2]); // população de tubarões
-    result.open("resPops=_"+(string)argv[1]+"_PopT=_"+(string)argv[2]+"_.csv");
-    result << "popS=" << popS<< "    popT="<<popT<<endl;
-    result << "tempoCC;tempoSC"<<endl;
+    int nAmo=atoi(argv[3]);
+    bool fex=exists_test("resultadoTese.csv");
+    result.open("resultadoTese.csv",fstream::app);
+    if(!fex)
+        result << "popS;popT;Gamma;Tccm;TCCsd;Tscm;TSCsd" <<endl;
     ti=clock();
 
-    double tempoCC[10], tempoSC[10];
+    double tempoCC[nAmo], tempoSC[nAmo];
 
     #pragma omp parallel for num_threads(4)
-    for(int si=1;si<=10;si++)
+    for(int si=1;si<=nAmo;si++)
     {
         ambiente oceanoCC(popS,popT,true); // com efeito cardume
         ambiente oceanoSC(popS,popT,false);// sem efeito
@@ -38,8 +45,23 @@ int main(int argc, char *argv[])
     }
     tf=clock();
     cout << "tempo:" << (double)(tf-ti)/CLOCKS_PER_SEC << endl;
-
-    for(int i=0; i<10; ++i)
-        result << tempoCC[i] << ";" << tempoSC[i] << endl;
+    double gamma=0;
+    int somaCC=0,somaCC2=0;
+    int somaSC=0,somaSC2=0;
+    for(int i=0; i<nAmo; ++i)
+    {
+        gamma+=(tempoCC[i]>tempoSC[i]);
+        somaCC+=tempoCC[i];
+        somaCC2+=(tempoCC[i]*tempoCC[i]);
+        somaSC+=tempoSC[i];
+        somaSC2+=(tempoSC[i]*tempoSC[i]);
+    }
+    double CCm=somaCC/nAmo;
+    double CCdesv=sqrt((double)somaCC2/nAmo-CCm*CCm);
+    double SCm=somaSC/nAmo;
+    double SCdesv=sqrt((double)somaSC2/nAmo-SCm*SCm);
+    result << (string)argv[1]+";"+(string)argv[2]+";" << (double)gamma/nAmo
+           << ";" <<CCm << ";" << CCdesv
+           << ";" <<SCm << ";" << SCdesv << endl;
     result.close();
 }
